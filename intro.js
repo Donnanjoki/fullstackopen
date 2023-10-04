c185c733e3e7eb200d6b161ac153a890f84b13db
Donna Njoki <njerinjoki2@gmail.com>
2023-10-04 22:15:07 +0300

    FullstackOpen- Part 0

---
+++ intro.js
@@ -0,0 +1,61 @@
+/*
+Source: https://fullstackopen.com/en/part0/fundamentals_of_web_apps
+
+PART 0: INTRO TO WEB DEVELOPMENT
+
+### First rule of web development ###
+
+>.Always keep the Developer Console Open on your web browser.
+
+
+### Hypertext Transfer Protocol(HTTP) GET ###
+
+>. The server and the web browser communicate with each other using the HTTP protocol.
+The network tab shows how the browser and server communicate. 
+
+>. HTTP is a stateless protocol, meaning the server does not keep any data(state) between two requests.
+
+>.So when a page is open, the following chain of events occurs.
+      1]] First, the browser sends a HTTP GET request to the server to fetch the HTML code of the page.
+      2]] If there is an image in the page, the img tag in the html prompts the browser to fetch the image
+      3]] The browser then renders the HTML page and the image on the screen
+      4]] Note, the HTML page begins to render before the image has been fetched from the server
+
+
+### Traditional Web Applications ###
+
+>. In traditional web applications, the browser is dubbed "dumb". It only fetchesHTML data from the server and all the application
+logic is on the server. A server can be created using JavaSpring, Python Flask or Ruby on Rails.
+
+
+### Event handlers and Callback functions ###
+
+>. Event handler functions are called callback functions. The application does not invoke the function itself, but the runtime environment,
+the browser, invokes the function at the appropriate time when the event has occurred.
+
+
+### Document Object Model or DOM ###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
